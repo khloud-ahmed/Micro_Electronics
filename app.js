505a8f7dd02d95e80b32bb9999ac50eb473a92fd
Khloud Ahmed <khloudabdelkawy24@gmail.com>
2026-02-14 12:23:17 +0200

    second

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 require("dotenv").config();
 
 const express =require("express");
-
+// hashed
+const bcrypt =require("bcrypt");
 
 const app = express ();
 
@@ -24,10 +25,83 @@ const Port= process.env.Port || 3000;
      }
  }
  dbConenction();
- 
+ // require models
+const User = require("./models/Users");
 
- 
+// POST route register
+app.post("/register", async (req, res) => {  
+    try {
+        // get data from request body
+        const { user_name, email, password, role } = req.body;
+        
+        // validate data
+        if (!user_name || !email || !password) {
+            return res.status(400).json({ msg: "invalid data" }); 
+        }
+        // check if user already exists
+        const existingUser = await User.findOne({ email });
+        if (existingUser) {
+            return res.status(400).json({ msg: "user already exists" });
+        }
+        //create new user 
+        const hashpassword = await bcrypt.hash(password, 10);
+        const user = await User.create ({
+            user_name,  
+            email,
+            password: hashpassword ,
+            role,
+        });
+        //res
+        res.status(201).json({ msg: "user registered successfully", 
+            data: user 
+        });
+        
+        
+     } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            msg: "Server error"
+        });
+    }
+});
+//login
+app.post("/login", async (req, res) => {  
+
+try {
+        const { email, password } = req.body;
+        if (!email || !password) {
+            return res.status(400).json({
+                msg: "Missing required data"
+            });
+        }
+        const user = await User.findOne({ email });
+        if (!user) {
+            return res.status(400).json({
+                msg: "user account not found"
+            });
+        }
+        const matchedPassword = await bcrypt.compare(password, user.password);
+
+        if (!matchedPassword) {
+            return res.status(400).json({
+                msg: "invalid password"
+            });
+        }
+        res.status(200).json({
+            msg:"success login"
+        })
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            msg: "Server error"
+        });
+    }
+ });
 // Run Server
 app.listen(Port, () => {
   console.log(`server Running At Port ${Port}`);
 });
+
+
+
